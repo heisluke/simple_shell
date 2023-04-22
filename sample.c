4cbf2d4dbf1b24bdb0d93a513a089b6c320ae4db
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-04-22 02:04:58 +0100

    betty checks

--- sample.c
+++ sample.c
@@ -1,23 +1,13 @@
-int sample_func(char suffix)
-{
-	int var;
+#include <stdio.h>
 
-	var = 0;
-	switch (suffix)
-	{
-	case 'G':
-	case 'g':
-		var = 30;
-		break;
-	case 'M':
-	case 'm':
-		var = 20;
-		break;
-	case 'K':
-	case 'k':
-		var = 10;
-	default:
-		break;
-	}
-	return (var);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    printf("Hello, world!\n");
+    return (0);
 }
